d16c87e9149d0163dbc034861a34d472c1027f44
aishyuu <ivanmatias1331@gmail.com>
2023-12-29 17:14:10 -0800

    Added hold feature and clear button

--- index.js
+++ index.js
@@ -6,10 +6,20 @@ const randomColorBtn = document.querySelector(".random-color-btn")
 const chooseColorBtn = document.querySelector(".choose-color")
 const paintText = document.querySelector(".paint-text")
 const randomColorText = document.querySelector(".random-color-text")
+const clearBtn = document.querySelector(".clear-btn")
 
 let color = colorWheel.value
 
 let mode = 'color'
+let holdColor = false
+
+document.body.addEventListener('mousedown', () => {
+    holdColor = true
+})
+
+document.body.addEventListener('mouseup', () => {
+    holdColor = false
+})
 
 colorWheel.addEventListener('change', (event) => {
     color = colorWheel.value
@@ -31,16 +41,26 @@ chooseColorBtn.addEventListener('click', () => {
     }
 })
 
+clearBtn.addEventListener('click', () => {
+    let allSquares = document.getElementsByClassName('etch-square')
+    for (let index = 0; index < allSquares.length; index++) {
+        const square = allSquares[index];
+        square.style.backgroundColor = 'white'
+    }
+})
+
 for (let index = 0; index < 16*16; index++) {
     let square = document.createElement('div')
     square.classList.add('etch-square')
     square.addEventListener('mouseover', () => {
-        if (mode === 'color') {
-            square.style.backgroundColor = color
-        }
-        if (mode === 'random') {
-            let randomColor = Math.floor(Math.random()*16777215).toString(16)
-            square.style.backgroundColor = '#' + randomColor
+        if(holdColor === true) {
+            if (mode === 'color') {
+                square.style.backgroundColor = color
+            }
+            if (mode === 'random') {
+                let randomColor = Math.floor(Math.random()*16777215).toString(16)
+                square.style.backgroundColor = '#' + randomColor
+            }
         }
     })
     etch.appendChild(square)
